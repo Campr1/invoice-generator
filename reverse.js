e074c75ea39ea4917aaae4924b167ca5a234b7c4
Rishard C <rishard.c@hotmail.com>
2026-03-17 22:55:29 -0400

    chore: remove unwanted files from repo

--- reverse.js
+++
@@ -1,7 +0,0 @@
-// Write a program that takes any number of command line arguments, all strings, and reverses them before outputting them one at a time to the console.
-const args = process.argv;
-
-const reverseStr = function(args) { 
-  args.split("").reverse().join("");
-}
-reverseStr(args);
